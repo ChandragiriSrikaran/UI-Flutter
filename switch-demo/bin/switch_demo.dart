51a872a96712cfc3e0044c4fcc2cfd3c3295ffe8
SrikaranChandragiri <154876496+ChandragiriSrikaran@users.noreply.github.com>
2025-05-29 22:02:16 +0530

    Delete switch-demo/bin directory

--- switch-demo/bin/switch_demo.dart
+++
@@ -1,8 +0,0 @@
-
-import 'package:switch_demo/switch_demo.dart' as switch_demo;
-import 'package:switch_demo/switch_demo1.dart' as switch_demo1;
-
-void main(List<String> arguments) {
-  print('Hello world: ${ switch_demo.switch1('pink')}!');
-   switch_demo1.switch1('blue');
-}
